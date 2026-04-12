c25baa26486218dc47b52afc595e90d5d6656fc8
Vinay Nair <vinaynair@wirelessprv-10-193-46-113.near.illinois.edu>
2026-04-12 14:31:43 -0500

    Created create event page

--- nobedevops/src/app/users/admin/page.tsx
+++ nobedevops/src/app/users/admin/page.tsx
@@ -1,12 +1,7 @@
 import { createClient } from "@/app/utils/supabase/server";
-import { randomBytes } from "crypto";
-import { cookies } from "next/headers";
-import AdminUI from "./adminUI";
-import AdminGuard from "./AdminGuard";
 import AdminDashboard, { AttendanceRow } from "./adminDashboard";
-import LogoutButton from "../login/logout"
-import BulkAddPage from "./bulkAdd/page";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminGuard from "./AdminGuard";
+import LogoutButton from "../login/logout";
 import Link from "next/link";
 
 
@@ -51,36 +46,6 @@ export default async function AdminPage() {
     recentAttendance = (recentRes.data ?? []) as AttendanceRow[];
   }
 
-  async function createEvent(formData: FormData) {
-    "use server";
-
-    const supabase = await createClient();
-
-    const name = String(formData.get("name") ?? "");
-    const event_type = String(formData.get("event_type") ?? "");
-    const points = Number(formData.get("points") ?? 0);
-    const is_mandatory =
-      String(formData.get("is_mandatory") ?? "false") === "true";
-    const date = String(formData.get("date") ?? "");
-
-    if (!name || !event_type || !date || Number.isNaN(points)) {
-      throw new Error("Missing/invalid fields");
-    }
-
-    const qr_code_secret = randomBytes(32).toString("hex");
-
-    const { error } = await supabase.from("events").insert({
-      name,
-      event_type,
-      points,
-      is_mandatory,
-      date,
-      qr_code_secret,
-    });
-
-    if (error) throw new Error(error.message);
-  }
-
   return (
     <AdminGuard>
       <div style={{ maxWidth: 520, margin: "40px auto" }}>
@@ -92,9 +57,6 @@ export default async function AdminPage() {
         />
         <div className="mx-auto max-w-6xl px-6 pb-10">
           <div className="rounded-lg border border-black/10 dark:border-white/20 p-6">
-            <AdminUI />
-          </div>
-            <div className="rounded-lg border border-black/10 dark:border-white/20 p-6">
             <LogoutButton />
           </div>
         </div>
@@ -104,8 +66,6 @@ export default async function AdminPage() {
           <Link href="/users/admin/reviewAbsence">Go to review absence</Link>
           <Link href="/users/admin/reviewMemberStats">Go to member stats</Link>
           <Link href="/users/admin/viewAllEvents">Go to view events</Link>
-
-
         </div>
       </div>
     </AdminGuard>
